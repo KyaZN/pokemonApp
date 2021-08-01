b11df99b7ac652332dda1493df7a42f5aadc82de
karina.quispe <karina.quispe@taximo.co>
2021-08-01 14:04:40 -0500

    Phase 1 - Basic HTML

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template , url_for , request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello Pokemon Trainer!"
+    if request.method == 'POST':
+        # task_content = request.form['content']
+        # new_task = Todo(content=task_content)
+
+        # try:
+        #     db.session.add(new_task)
+        #     db.session.commit()
+        #     return redirect('/')
+        # except:
+        #     return 'There was an issue adding your task'
+        pass
+
+    else:
+        # tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
